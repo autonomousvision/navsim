d7a0c36e32fb3a8fbeabcd9cce58cbcba822b876
Marcel Hallgarten <marcel.hallgarten@de.bosch.com>
2024-03-20 15:10:29 +0100

    Add script & instructions for warmup submission

---
+++ docs/submission.md
@@ -0,0 +1,16 @@
+# Submitting to the Leaderboard
+
+Navsim comes with an official leaderboard on HuggingFace that remains open and prevents ambiguity in metric definitions between projects.
+To submit to the evaluation server you need to create a pickle file that contains a trajectory for each test scenario.
+Navsim provides a script to create such a pickle file. 
+
+Have a look at `run_cv_submission_evaluation.sh`: this file creates the pickle file for a simplistic Constant-Velocity agent.
+You can run it for your agent, by replacing the `agent` override.
+
+### Warm-up phase
+During the warm-up phase the evaluation on the submission server is based on the `mini` split.
+This allows you to test your method and get familiar with the devkit and the submisison procedure.
+You should be able to obtain the same evaluation results as on the server, by running the evaluation locally with the `warmup_test` scene-filter. To do so, use the override `scene_filter=warmup_test` when executing the script to run the pdm-scoring (e.g.,  `run_cv_pdm_score_evaluation.sh` for the constant-velocity agent).
+
+### Test phase
+> **More details and instructions soon**
---
+++ navsim/planning/script/run_submission_evaluation.py
@@ -0,0 +1,89 @@
+from tqdm import tqdm
+import traceback
+import pickle
+import hydra
+from hydra.utils import instantiate
+from omegaconf import DictConfig
+import os
+
+from pathlib import Path
+from typing import Dict
+import logging
+
+from navsim.agents.abstract_agent import AbstractAgent
+from navsim.common.dataclasses import Trajectory, SceneFilter
+from navsim.common.dataloader import SceneLoader
+
+
+logger = logging.getLogger(__name__)
+
+# TODO: provide a separate config
+CONFIG_PATH = "config/pdm_scoring"
+CONFIG_NAME = "default_run_submission_evaluation"
+
+
+@hydra.main(config_path=CONFIG_PATH, config_name=CONFIG_NAME)
+def main(cfg: DictConfig) -> None:
+    agent = instantiate(cfg.agent)
+    data_path = Path(cfg.navsim_log_path)
+    sensor_blobs_path = Path(cfg.sensor_blobs_path)
+    save_path = Path(cfg.output_dir)
+    scene_filter = instantiate(cfg.scene_filter)
+
+    run_test_evaluation(
+        agent=agent,
+        scene_filter=scene_filter,
+        data_path=data_path,
+        sensor_blobs_path=sensor_blobs_path,
+        save_path=save_path,
+    )
+
+def run_test_evaluation(
+    agent: AbstractAgent,
+    scene_filter: SceneFilter,
+    data_path: Path,
+    sensor_blobs_path: Path,
+    save_path: Path
+) -> None:
+    """
+    Function to create the output file for evaluation of an agent on the testserver
+    :param agent: Agent object
+    :param data_path: pathlib path to navsim logs
+    :param sensor_blobs_path: pathlib path to sensor blobs
+    :param save_path: pathlib path to folder where scores are stored as .csv
+    """    
+    if agent.requires_scene:
+        raise ValueError(
+        """
+            In evaluation, no access to the annotated scene is provided, but only to the AgentInput. 
+            Thus, agent.requires_scene has to be False for the agent that is to be evaluated.
+        """
+    ) 
+    logger.info("Building Agent Input Loader")
+    input_loader = SceneLoader(
+        data_path=data_path,
+        scene_filter=scene_filter,
+        sensor_blobs_path=sensor_blobs_path,
+        sensor_modalities=agent.get_sensor_modalities()
+    )
+    agent.initialize()
+
+    output: Dict[str, Trajectory] = {}
+    for token in tqdm(input_loader, desc="Running evaluation"):
+        try:
+            agent_input = input_loader.get_agent_input_from_token(token)
+            trajectory = agent.compute_trajectory(agent_input)
+            output.update({"token": trajectory})
+        except Exception as e:
+            logger.warning(f"----------- Agent failed for token {token}:")
+            traceback.print_exc()
+
+    # pickle and save dict
+    filename = os.path.join(save_path, "submission.pkl")
+    with open(filename, 'wb') as file:
+        pickle.dump(output, file)
+    logger.info(f"Your submission filed was saved to {filename}")
+
+
+if __name__ == "__main__":
+    main()
