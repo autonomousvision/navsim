8af06bd77c58396c675cc5f7e3255efa6f3ac7cf
Daniel Dauner <daniel.dauner@gmail.com>
2024-04-24 12:47:33 +0200

    Update download_navtrain.sh

--- download/download_navtrain.sh
+++ download/download_navtrain.sh
@@ -1,19 +1,26 @@
-for split in {1..5}; do
+wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/openscene-v1.1/openscene_metadata_trainval.tgz
+tar -xzf openscene_metadata_trainval.tgz
+rm openscene_metadata_trainval.tgz
+mv openscene-v1.1/meta_datas trainval_navsim_logs
+rm -r openscene-v1.1
+
+mkdir -p trainval_sensor_blobs/trainval
+for split in {1..4}; do
     wget https://s3.eu-central-1.amazonaws.com/avg-projects-2/navsim/navtrain_current_${split}.tgz
     echo "Extracting file navtrain_current_${split}.tgz"
     tar -xzf navtrain_current_${split}.tgz
     rm navtrain_current_${split}.tgz
 
-    mv navtrain_current_${split} navtrain
-    rm -r navtrain_current_${split}
+    mv -v current_split_${split}/* trainval_sensor_blobs/trainval
+    rm -r current_split_${split}
 done
 
-for split in {1..5}; do
+for split in {1..4}; do
     wget https://s3.eu-central-1.amazonaws.com/avg-projects-2/navsim/navtrain_history_${split}.tgz
     echo "Extracting file navtrain_history_${split}.tgz"
     tar -xzf navtrain_history_${split}.tgz
     rm navtrain_history_${split}.tgz
 
-    mv navtrain_history_${split} navtrain
-    rm -r navtrain_history_${split}
+    mv -v history_split_${split}/* trainval_sensor_blobs/trainval
+    rm -r history_split_${split}
 done
