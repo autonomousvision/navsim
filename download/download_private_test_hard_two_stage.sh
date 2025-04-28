eff6aa3af82cacfdd75d6ffd24cc942de1392e56
Wei Cao <dave.caowei@outlook.com>
2025-04-28 20:31:29 +0200

    Update sensor download link in download_private_test_hard_two_stage.sh

--- download/download_private_test_hard_two_stage.sh
+++ download/download_private_test_hard_two_stage.sh
@@ -2,6 +2,12 @@ wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-
 tar -xzf navsim_v2.2_private_test_hard_two_stage.tar.gz
 rm navsim_v2.2_private_test_hard_two_stage.tar.gz
 
+wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/openscene-v1.1/openscene_sensor_private_test_hard.tar.gz
+tar -xzf openscene_sensor_private_test_hard.tar.gz
+rm openscene_sensor_private_test_hard.tar.gz
+mv openscene-v1.1/sensor_blobs/ private_test_hard_navsim_sensor
+rm -r openscene-v1.1
+
 wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/openscene-v1.1/openscene_metadata_private_test_hard.tar.gz
 tar -xzf openscene_metadata_private_test_hard.tar.gz
 rm openscene_metadata_private_test_hard.tar.gz
