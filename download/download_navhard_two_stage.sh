2af9db8212cc2dbdfadf22d05b42eae817c09501
mh0797 <marcel.hallgarten@web.de>
2025-04-28 07:34:16 +0000

    version2.2

--- download/download_navhard_two_stage.sh
+++ download/download_navhard_two_stage.sh
@@ -1,10 +1,9 @@
-wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.1.1_navhard_two_stage_curr_sensors.tar.gz
-wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.1.1_navhard_two_stage_hist_sensors.tar.gz
-wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.1.1_navhard_two_stage_scene_pickles.tar.gz
-
-tar -xzvf navsim_v2.1.1_navhard_two_stage_curr_sensors.tar.gz
-tar -xzvf navsim_v2.1.1_navhard_two_stage_hist_sensors.tar.gz
-tar -xzvf navsim_v2.1.1_navhard_two_stage_scene_pickles.tar.gz
-rm navsim_v2.1.1_navhard_two_stage_curr_sensors.tar.gz
-rm navsim_v2.1.1_navhard_two_stage_hist_sensors.tar.gz
-rm navsim_v2.1.1_navhard_two_stage_scene_pickles.tar.gz
+wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz
+wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz
+wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz
+tar -xzvf navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz
+tar -xzvf navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz
+tar -xzvf navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz
+rm navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz
+rm navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz
+rm navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz
